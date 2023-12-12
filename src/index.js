1f9cb9cfac86b01d6fd863ad1494049139459b4e
lucca-sa <scherer.lucca@gmail.com>
2023-12-12 00:42:56 -0300

    Login Page Styled

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './pages/Login'
+import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
     <Login />
+    <GlobalStyles />
   </>
 )
