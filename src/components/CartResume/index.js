a84dd1b9349d24fdaec3ea616cbe57364cd639e3
lucca-sa <scherer.lucca@gmail.com>
2024-02-25 21:19:15 -0300

    feat: Add Cart Resume

---
+++ src/components/CartResume/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Button } from '../Button'
+import { Container } from './styles'
+
+export function CartResume() {
+  return (
+    <div>
+      <Container>
+        <div className="container-top">
+          <h2 className="title">Resumo do pedido</h2>
+          <p className="items">Itens</p>
+          <p className="items-price">R$ 10,00</p>
+          <p className="delivery-fee">Taxa de entrega</p>
+          <p className="delivery-price">R$ 10,00</p>
+        </div>
+        <div className="container-bottom">
+          <p>Total</p>
+          <p>R$ 20,00</p>
+        </div>
+      </Container>
+      <Button style={{ width: '100%', marginTop: '30px' }}>
+        Confirmar pedido!
+      </Button>
+    </div>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 export * from './Button'
 export * from './CartItems'
+export * from './CartResume'
 export * from './CategoryCarousel'
 export * from './OfferCarousel'
 export * from './ProductsCard'
